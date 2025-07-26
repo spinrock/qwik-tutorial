e4d2424ef5eb00ce9c93ca0e6b3a38bbff94cf3c
satoshi.fukai <chemistreal2@gmail.com>
2025-07-26 23:46:19 +0900

    STEP3: Posting Data to the Server

--- src/routes/joke/index.tsx
+++ src/routes/joke/index.tsx
@@ -1,5 +1,10 @@
 import { component$ } from "@builder.io/qwik";
-import { routeLoader$ } from '@builder.io/qwik-city';
+import { routeLoader$, Form, routeAction$ } from '@builder.io/qwik-city';
+
+export const useJokeVoteAction = routeAction$((props) => {
+  // Leave it as an exercise for the reader to implement this.
+  console.log('VOTE', props);
+});
 
 export const useDadJoke = routeLoader$(async () => {
   const response = await fetch('https://icanhazdadjoke.com/', {
@@ -15,10 +20,16 @@ export const useDadJoke = routeLoader$(async () => {
 export default component$(() => {
   // Calling our `useDadJoke` hook, will return a reactive signal to the loaded data.
   const dadJokeSignal = useDadJoke();
+  const favoriteJokeAction = useJokeVoteAction();
   
   return (
     <section class="section bright">
       <p>{dadJokeSignal.value.joke}</p>
+      <Form action={favoriteJokeAction}>
+        <input type="hidden" name="jokeID" value={dadJokeSignal.value.id} />
+        <button name="vote" value="up">👍</button>
+        <button name="vote" value="down">👎</button>
+      </Form>
     </section>
   );
 });
